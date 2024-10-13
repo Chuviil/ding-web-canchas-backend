3eec499cbed7cf2049dff8477944ec1564adf6e7
Sebastian Quito <josue.quito@udla.edu.ec>
2024-10-13 11:34:25 -0500

    Basic Auth module to hable auth on Next

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { CourtsModule } from './courts/courts.module';
 import { PrismaService } from './prisma/prisma.service';
 import { UsersModule } from './users/users.module';
+import { AuthService } from './auth/auth.service';
+import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [CourtsModule, UsersModule],
-  controllers: [],
-  providers: [PrismaService],
+  imports: [CourtsModule, UsersModule, AuthModule],
+  controllers: [AuthController],
+  providers: [PrismaService, AuthService],
 })
 export class AppModule {}
